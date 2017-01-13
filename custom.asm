fa145768ae7e8bc9e7150add8035e104bcfc3e1b
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2017-01-13 20:54:20 +0100

    replace `t' with smiley

--- custom.asm
+++ custom.asm
@@ -1,3 +1,4 @@
+// converted from BASIC from the Commodore 64 Programmer's reference guide page 110
 .pc = $0801 "Basic Upstart Program"
 :BasicUpstart($0810)
 
@@ -21,10 +22,10 @@
 t:
 	txa
 	lda $d000,x
-	and #%10101010
+	//and #%10101010
 	sta $3000,x
 	lda $d100,x
-	and #%10101010
+	//and #%10101010
 	sta $3100,x
 	inx
 	bne t
@@ -36,6 +37,14 @@ t:
 	lda 1
 	ora #4
 	sta 1
+	// replace `t' with smiley
+	ldx #0
+l:
+	lda tbl,x
+	sta $30a0,x
+	inx
+	cpx #8
+	bne l
 	// update vic to read character set from ram
 	lda $d018
 	and #$f0
@@ -44,3 +53,12 @@ t:
 	sta $d018
 loop:
 	jmp loop
+tbl:
+	.byte %00111100
+	.byte %01000010
+	.byte %10100101
+	.byte %10000001
+	.byte %10100101
+	.byte %10011001
+	.byte %01000010
+	.byte %00111100
