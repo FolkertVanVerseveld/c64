d71f77eece4564eb88462a11d748bfa9d8597ec0
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2017-11-05 01:17:42 +0100

    tools/diss: opcode table
    several programs can now be dissassembled, but we need to add more
    checks to make sure `.prg's are parsed properly. the file size is now
    printed in hexadecimal by default followed by the decimal value.

--- tools/diss.c
+++ tools/diss.c
@@ -17,6 +17,63 @@
 #define T_D64 3
 #define T_G64 4
 
+#define O_UNK 0
+#define O_IMP 1
+#define O_IMM 2
+#define O_ZP  3
+#define O_ZPX 4
+#define O_ZPY 5
+#define O_IZX 6
+#define O_IZY 7
+#define O_ABS 8
+#define O_ABX 9
+#define O_ABY 10
+#define O_IND 11
+#define O_REL 12
+
+struct op {
+	unsigned type;
+	const char *name;
+} optbl[256] = {
+	{O_IMP, "BRK"}, {O_IZX, "ORA"}, {O_IMP, "KIL"}, {O_IZX, "SLO"}, {O_ZP, "NOP"}, {O_ZP, "ORA"}, {O_ZP, "ASL"}, {O_ZP, "SLO"},
+	{O_IMP, "PHP"}, {O_IMM, "ORA"}, {O_IMP, "ASL"}, {O_IMM, "ANC"}, {O_ABS, "NOP"}, {O_ABS, "ORA"}, {O_ABS, "ASL"}, {O_ABS, "SLO"},
+	{O_REL, "BPL"}, {O_IZY, "ORA"}, {O_IMP, "KIL"}, {O_IZY, "SLO"}, {O_ZPX, "NOP"}, {O_ZPX, "ORA"}, {O_ZPX, "ASL"}, {O_ZPX, "SLO"},
+	{O_IMP, "CLC"}, {O_ABY, "ORA"}, {O_IMP, "NOP"}, {O_ABY, "SLO"}, {O_ABX, "NOP"}, {O_ABX, "ORA"}, {O_ABX, "ASL"}, {O_ABX, "SLO"},
+	{O_ABS, "JSR"}, {O_IZX, "AND"}, {O_IMP, "KIL"}, {O_IZX, "RLA"}, {O_ZP, "BIT"}, {O_ZP, "AND"}, {O_ZP, "ROL"}, {O_ZP, "RLA"},
+	{O_IMP, "PLP"}, {O_IMM, "AND"}, {O_IMP, "ROL"}, {O_IMM, "ANC"}, {O_ABS, "BIT"}, {O_ABS, "AND"}, {O_ABS, "ROL"}, {O_ABS, "RLA"},
+	{O_REL, "BMI"}, {O_IZY, "AND"}, {O_IMP, "KIL"}, {O_IZY, "RLA"}, {O_ZPX, "NOP"}, {O_ZPX, "AND"}, {O_ZPX, "ROL"}, {O_ZPX, "RLA"},
+	{O_IMP, "SEC"}, {O_ABY, "AND"}, {O_IMP, "NOP"}, {O_ABY, "RLA"}, {O_ABX, "NOP"}, {O_ABX, "AND"}, {O_ABX, "ROL"}, {O_ABX, "RLA"},
+	{O_IMP, "RTI"}, {O_IZX, "EOR"}, {O_IMP, "KIL"}, {O_IZY, "SRE"}, {O_ZP, "NOP"}, {O_ZP, "EOR"}, {O_ZP, "LSR"}, {O_ZP, "SRE"},
+	{O_IMP, "PHA"}, {O_IMM, "EOR"}, {O_IMP, "LSR"}, {O_IMM, "ALR"}, {O_ABS, "JMP"}, {O_ABS, "EOR"}, {O_ABS, "LSR"}, {O_ABS, "SRE"},
+	{O_REL, "BVC"}, {O_IZY, "EOR"}, {O_IMP, "KIL"}, {O_IZY, "SRE"}, {O_ZPX, "NOP"}, {O_ZPX, "EOR"}, {O_ZPX, "LSR"}, {O_ZPX, "SRE"},
+	{O_IMP, "CLI"}, {O_ABY, "EOR"}, {O_IMP, "NOP"}, {O_ABY, "SRE"}, {O_ABX, "NOP"}, {O_ABX, "EOR"}, {O_ABX, "LSR"}, {O_ABX, "SRE"},
+	{O_IMP, "RTS"}, {O_IZX, "ADC"}, {O_IMP, "KIL"}, {O_IZY, "RRA"}, {O_ZP, "NOP"}, {O_ZP, "ADC"}, {O_ZP, "ROR"}, {O_ZP, "RRA"},
+	{O_IMP, "PLA"}, {O_IMM, "ADC"}, {O_IMP, "ROR"}, {O_IMM, "ARR"}, {O_IND, "JMP"}, {O_ABS, "ADC"}, {O_ABS, "ROR"}, {O_ABS, "RRA"},
+	{O_REL, "BVS"}, {O_IZY, "ADC"}, {O_IMP, "KIL"}, {O_IZY, "RRA"}, {O_ZPX, "NOP"}, {O_ZPX, "ADC"}, {O_ZPX, "ROR"}, {O_ZPX, "RRA"},
+	{O_IMP, "SEI"}, {O_ABY, "ADC"}, {O_IMP, "NOP"}, {O_ABY, "RRA"}, {O_ABX, "NOP"}, {O_ABX, "ADC"}, {O_ABX, "ROR"}, {O_ABX, "RRA"},
+	{O_IMM, "NOP"}, {O_IZX, "STA"}, {O_IMM, "NOP"}, {O_IZX, "SAX"}, {O_ZP, "STY"}, {O_ZP, "STA"}, {O_ZP, "STX"}, {O_ZP, "SAX"},
+	{O_IMP, "DEY"}, {O_IMM, "NOP"}, {O_IMP, "TXA"}, {O_IMM, "XAA"}, {O_ABS, "STY"}, {O_ABS, "STA"}, {O_ABS, "STX"}, {O_ABS, "SAX"},
+	{O_REL, "BCC"}, {O_IZY, "STA"}, {O_IMP, "KIL"}, {O_IZY, "AHX"}, {O_ZPX, "STY"}, {O_ZPX, "STA"}, {O_ZPY, "STX"}, {O_ZPY, "SAX"},
+	{O_IMP, "TYA"}, {O_ABY, "STA"}, {O_IMP, "TXS"}, {O_ABY, "TAS"}, {O_ABX, "SHY"}, {O_ABX, "STA"}, {O_ABY, "SHX"}, {O_ABY, "AHX"},
+	{O_IMM, "LDY"}, {O_IZX, "LDA"}, {O_IMM, "LDX"}, {O_IZX, "LAX"}, {O_ZP, "LDY"}, {O_ZP, "LDA"}, {O_ZP, "LDX"}, {O_ZP, "LAX"},
+	{O_IMP, "TAY"}, {O_IMM, "LDA"}, {O_IMP, "TAX"}, {O_IMM, "LAX"}, {O_ABS, "LDY"}, {O_ABS, "ABS"}, {O_ABS, "LDX"}, {O_ABS, "LAX"},
+	{O_REL, "BCS"}, {O_IZY, "LDA"}, {O_IMM, "KIL"}, {O_IZY, "LAX"}, {O_ZPX, "LDY"}, {O_ZPX, "LDA"}, {O_ZPY, "LDX"}, {O_ZPY, "LAX"},
+	{O_IMM, "CLV"}, {O_ABY, "LDA"}, {O_IMP, "TSX"}, {O_ABY, "LAS"}, {O_ABX, "LDY"}, {O_ABX, "LDA"}, {O_ABY, "LDX"}, {O_ABY, "LAX"},
+	{O_IMM, "CPY"}, {O_IZX, "CMP"}, {O_IMM, "NOP"}, {O_IZX, "DCP"}, {O_ZP, "CPY"}, {O_ZP, "CMP"}, {O_ZP, "DEC"}, {O_ZP, "DCP"},
+	{O_IMP, "INY"}, {O_IMM, "CMP"}, {O_IMP, "DEX"}, {O_IMM, "AXS"}, {O_ABS, "CPY"}, {O_ABS, "CMP"}, {O_ABS, "DEC"}, {O_ABS, "DCP"},
+	{O_REL, "BNE"}, {O_IZY, "CMP"}, {O_IMP, "KIL"}, {O_IZY, "DCP"}, {O_ZPX, "NOP"}, {O_ZPX, "CMP"}, {O_ZPX, "DEC"}, {O_ZPX, "DCP"},
+	{O_IMP, "CLD"}, {O_ABY, "CMP"}, {O_IMP, "NOP"}, {O_ABY, "DCP"}, {O_ABX, "NOP"}, {O_ABX, "CMP"}, {O_ABX, "DEC"}, {O_ABX, "DCP"},
+	{O_IMM, "CPX"}, {O_IZX, "SBC"}, {O_IMM, "NOP"}, {O_IZX, "ISC"}, {O_ZP, "CPX"}, {O_ZP, "SBC"}, {O_ZP, "INC"}, {O_ZP, "ISC"},
+	{O_IMM, "INX"}, {O_IMM, "SBC"}, {O_IMP, "NOP"}, {O_IMM, "SBC"}, {O_ABS, "CPX"}, {O_ABS, "SBC"}, {O_ABS, "INC"}, {O_ABS, "ISC"},
+	{O_REL, "BEQ"}, {O_IZY, "SBC"}, {O_IMP, "KIL"}, {O_IZY, "ISC"}, {O_ZPX, "NOP"}, {O_ZPX, "SBC"}, {O_ZPX, "INC"}, {O_ZPX, "ISC"},
+	{O_IMP, "SED"}, {O_ABY, "SBC"}, {O_IMP, "NOP"}, {O_ABY, "ISC"}, {O_ABX, "NOP"}, {O_ABX, "SBC"}, {O_ABX, "INC"}, {O_ABX, "ISC"},
+};
+
+unsigned opl[O_REL + 1] = {
+	1, 1, 2, 2, 2, 2, 2, 2,
+	3, 3, 3, 3, 2,
+};
+
 struct bfile {
 	int fd;
 	char name[COL];
@@ -111,6 +168,12 @@ static void dump(const void *ptr, size_t n, unsigned row)
 	}
 }
 
+#define chkn(x) \
+	if (i + x > n) {\
+		printf(".byte $%02X\n", data[i++]);\
+		continue;\
+	}
+
 static int dump_prg(const void *ptr, size_t n, unsigned row)
 {
 	const unsigned char *data;
@@ -124,7 +187,27 @@ static int dump_prg(const void *ptr, size_t n, unsigned row)
 
 	load = data[1] << 8 | data[0];
 	printf("* = $%X \"start\"\n", load);
-	dump(data + 2, n - 2, row);
+
+	for (size_t i = 2; i < n;) {
+		const struct op *o = &optbl[data[i]];
+		unsigned l = opl[o->type];
+		chkn(l);
+		switch(o->type) {
+		case O_IMP: puts(o->name); break;
+		case O_IMM: printf("%s $%02X\n", o->name, data[i + 1]); break;
+		case O_ZP : printf("%s $%02X\n", o->name, data[i + 1]); break;
+		case O_ZPX: printf("%s $%02X,X\n", o->name, data[i + 1]); break;
+		case O_ZPY: printf("%s $%02X,Y\n", o->name, data[i + 1]); break;
+		case O_IZX: printf("%s ($%02X, X)\n", o->name, data[i + 1]); break;
+		case O_IZY: printf("%s ($%02X), Y\n", o->name, data[i + 1]); break;
+		case O_ABS: printf("%s $%04X\n", o->name, data[i + 2] << 8 | data[i + 1]); break;
+		case O_ABX: printf("%s $%04X,X\n", o->name, data[i + 2] << 8 | data[i + 1]); break;
+		case O_ABY: printf("%s $%04X,Y\n", o->name, data[i + 2] << 8 | data[i + 1]); break;
+		case O_REL: printf("%s $%02X\n", o->name, data[i + 1]); break;
+		default: printf(".byte $%02X\n", data[i]); break;
+		}
+		i += l;
+	}
 
 	return 0;
 }
@@ -134,7 +217,7 @@ static int diss(const struct bfile *f)
 	int ret = 1;
 	printf(
 		"// name: %s (type: %u)\n"
-		"// size: %zu (0x%zX)\n",
+		"// size: $%zX (%zu)\n",
 		f->name, f->type,
 		f->st.st_size, f->st.st_size
 	);
