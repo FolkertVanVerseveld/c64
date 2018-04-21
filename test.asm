9dbf998e14cff560d197436baf230755f07949fb
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2018-04-21 20:49:00 +0200

    completed hcc hacking day
    also reindented test.asm

--- test.asm
+++ test.asm
@@ -13,103 +13,95 @@ BasicUpstart2(main)
 main:
 // wait for next frame
 !l:
-    bit $d011
-    bpl !l-
+	bit $d011
+	bpl !l-
 !l:
-    bit $d011
-    bmi !l-
-    // draw next frame
+	bit $d011
+	bmi !l-
+	// draw next frame
 
-    // `plot' character on screen
+	// `plot' character on screen
+//scr_ptr:
+	//inc screen
+	lda #160
 scr_ptr:
-    inc screen
-    //lda #160
-//!ptr:
-    //sta screen
+	sta screen
 
 ///////// TODO check if table line entry has been completed
 
-    // check if scr_ptr == current_line_entry
-    lda scr_ptr + 1
+	// check if scr_ptr == current_line_entry
+	lda scr_ptr + 1
 tbl_low_ptr:
-    cmp tbl_line
-    bne !skip+
-    lda scr_ptr + 2
+	cmp tbl_line
+	bne !skip+
+	lda scr_ptr + 2
 tbl_high_ptr:
-    cmp tbl_line + 1
-    bne !skip+
+	cmp tbl_line + 1
+	bne !skip+
 
-    // get next table entry: update dir_ptr and tbl_low/high_ptr
+	// get next table entry: update dir_ptr and tbl_low/high_ptr
 
-    // update dir_ptr
-    inc dir_ptr + 1
-    bne !l+
-    inc dir_ptr + 2
+	// update dir_ptr
+	inc dir_ptr + 1
+	bne !l+
+	inc dir_ptr + 2
 !l:
-    // TODO if dir_ptr == 0, reset
-
-
-
-    // tbl_low_ptr += 2, tbl_high_ptr += 2
-    lda tbl_low_ptr + 1
-    clc
-    adc #2
-    sta tbl_low_ptr + 1
-    bne !l+
-    inc tbl_low_ptr + 2
-!l:
-    lda tbl_high_ptr + 1
-    clc
-    adc #2
-    sta tbl_high_ptr + 1
-    bne !l+
-    inc tbl_high_ptr + 2
+	// TODO if dir_ptr == 0, quit
+	lda dir_ptr + 1
+	sta !ptr+ + 1
+	lda dir_ptr + 2
+	sta !ptr+ + 2
+!ptr:
+	lda tbl_dir
+	bne !l+
+	// DONE
+	rts
 !l:
 
-
-    // TODO reset tbl_low_ptr/tbl_high_ptr if 0
-
-    jmp main
+	// tbl_low_ptr += 2, tbl_high_ptr += 2
+	lda tbl_low_ptr + 1
+	clc
+	adc #2
+	sta tbl_low_ptr + 1
+	bne !l+
+	inc tbl_low_ptr + 2
+!l:
+	lda tbl_high_ptr + 1
+	clc
+	adc #2
+	sta tbl_high_ptr + 1
+	bne !l+
+	inc tbl_high_ptr + 2
+!l:
+	// TODO reset tbl_low_ptr/tbl_high_ptr if 0
+	jmp main
 
 !skip:
-
-    // update screen pointer
-    lda scr_ptr + 1
-    sta num1lo
-    lda scr_ptr + 2
-    sta num1hi
+	// update screen pointer
+	lda scr_ptr + 1
+	sta num1lo
+	lda scr_ptr + 2
+	sta num1hi
 dir_ptr:
-    lda tbl_dir
-    sta delta
-    jsr adds8_16
-    // store result
-    lda num1lo
-    sta scr_ptr + 1
-    lda num1hi
-    sta scr_ptr + 2
+	lda tbl_dir
+	sta delta
+	jsr adds8_16
+	// store result
+	lda num1lo
+	sta scr_ptr + 1
+	lda num1hi
+	sta scr_ptr + 2
 
-    jmp main
+	jmp main
 
-// screen pointer direction table
-// resets when item is 0
-tbl_dir:
-    //.byte 40, 40, -1, -40, 0
-    .byte 1, 40, -1, -40, 1, 0
-    //.byte 1, 0
-    // line table
+// sample tables
 
-// each entry determines when screen pointer changes direction
-tbl_line:
-    //.word screen + 4 * 40
-    .word screen + 30
-    .word screen + 15 * 40 + 30
-    .word screen + 15 * 40
-    .word screen + 4 * 40
-    .word screen + 4 * 40 + 10
-tbl_end:
+//#import "diagonals.tbl"
+#import "random.tbl"
+//#import "rectangles.tbl"
 
 adds8_16:
-    ldx #$00 // implied high byte of delta
+	ldx #$00 // implied high byte of delta
 	lda delta // the signed 8-bit number
 	bpl !l+
 	dex // high byte becomes $ff to reflect negative delta
@@ -120,4 +112,4 @@ adds8_16:
 	txa
 	adc num1hi
 	sta num1hi
-    rts
+	rts
--- watch.sh
+++ watch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :; do
-	inotifywait -qre modify test.asm
+	inotifywait -qre modify test.asm *.tbl
 	java -jar /home/methos/bin/kickass-4.4/KickAss.jar test.asm
 	python sock.py p test.prg 192.168.2.64
 done
