7e3d3079201b37d811bf1d21df4b312b82c52a0d
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2018-12-13 00:44:28 +0100

    fix lfsr algorithm

--- lfsr.asm
+++ lfsr.asm
@@ -15,20 +15,22 @@ BasicUpstart2(main)
 }
 
 main:
-	lda #$35
-	sta $1
-
 	// initialize and show start state
 	jsr lfsr_init
 	lda lfsr_low
-	sta screen
+	sta screen + 0 * 40 + 0
 	lda lfsr_high
-	sta screen + 1
+	sta screen + 0 * 40 + 1
 	jsr lfsr_next
 	lda lfsr_low
-	sta screen + 40
+	sta screen + 1 * 40 + 0
 	lda lfsr_high
-	sta screen + 1 + 40
+	sta screen + 1 * 40 + 1
+	jsr lfsr_next
+	lda lfsr_low
+	sta screen + 2 * 40 + 0
+	lda lfsr_high
+	sta screen + 2 * 40 + 1
 	jmp *
 
 lfsr_init:
@@ -38,46 +40,27 @@ lfsr_init:
 	sta lfsr_high
 	rts
 
+	// bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1
+	// lfsr = (lfsr >> 1) | (bit << 9)
 lfsr_next:
 	break()
-	lda lfsr_low // a = lfsr
+	lda lfsr_low // bit = lfsr
 	sta lfsr_bit // lfsr_bit = lfsr
 	lsr lfsr_bit // lfsr_bit >>= 1
 	lsr lfsr_bit // lfsr_bit >>= 1
-	eor lfsr_bit // a ^= lfsr_bit
+	eor lfsr_bit // bit ^= lfsr_bit
 	lsr lfsr_bit // lfsr_bit >>= 1
-	eor lfsr_bit // a ^= lfsr_bit
+	eor lfsr_bit // bit ^= lfsr_bit
 	lsr lfsr_bit // lfsr_bit >>= 1
 	lsr lfsr_bit // lfsr_bit >>= 1
-	eor lfsr_bit // a ^= lfsr_bit
+	eor lfsr_bit // bit ^= lfsr_bit
+	clc
 	ror lfsr_high // lfsr_high >>= 1
 	ror lfsr_low  // lfsr_low >>= 1
-	and #$01 // a &= 1
-	bcc !s+
+	and #$01 // bit &= 1
+	bcc !s+ // add bit << 9 if bit ninzero
 	lda #$02
 	ora lfsr_high
 	sta lfsr_high
 !s:
-	rts
-
-	// bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1
-	// lfsr = (lfsr >> 1) | (bit << 9)
-	lda lfsr_low
-	sta lfsr_bit
-	// lfsr >> 2
-	ror lfsr_bit
-	ror lfsr_bit
-	// (lfsr >> 0) ^ (lfsr >> 2)
-	eor lfsr_bit
-	ror lfsr_bit
-	// (lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3)
-	eor lfsr_bit
-	ror lfsr_bit
-	ror lfsr_bit
-	// (lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)
-	eor lfsr_bit
-	// & 1
-	and #$01
-	// bit is computed
-	// TODO compute new lfsr
 	rts
